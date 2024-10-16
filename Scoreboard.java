97e47e761081bdffedcffe0cc9412d1a74a1d3ab
aymantalukder <aymant@nycstudents.net>
2024-10-16 12:19:09 -0400

    Add files via upload

--- Scoreboard.java
+++ Scoreboard.java
@@ -6,16 +6,35 @@ public class Scoreboard{
     private String activeTeam;
 
 
-    public Scoreboard(String one, String two)
-    {
+    public Scoreboard(String one, String two) {
         teamOne = one;
         teamTwo = two;
         activeTeam = teamOne;
+        teamTwoScore = 0;
+        teamOneScore = 0;
     }
 
     public String getScore()
     {
         return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
     }
+    public void recordPlay(int score){
+        if(score==0){
+            if(activeTeam==teamOne){
+                activeTeam=teamTwo;
+            }
+            else{
+                activeTeam=teamOne;
+            }
+        }
+        else{
+            if(activeTeam==teamOne){
+                teamOneScore+=score;
+            }
+            else{
+                teamTwoScore+=score;
+            }
+        }
+        }
 
-}
+    }
